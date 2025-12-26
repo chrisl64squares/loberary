f69c6edc0e6c71458b2f26491d75236d5fb9dfbe
chrisl64squares <chrisl64squares@gmail.com>
2025-12-26 13:36:44 -0800

    Quartz sync: Dec 26, 2025, 1:36 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Loberary",
+    pageTitle: "The Loberary",
     pageTitleSuffix: " | chrisl64",
     enableSPA: true,
     enablePopovers: true,
@@ -28,6 +28,30 @@ const config: QuartzConfig = {
         body: "Public Sans",
         code: "Source Code Pro",
       },
+      // colors: {
+      //   lightMode: {
+      //     light: "#faf8f8",
+      //     lightgray: "#e5e5e5",
+      //     gray: "#b8b8b8",
+      //     darkgray: "#4e4e4e",
+      //     dark: "#2b2b2b",
+      //     secondary: "#284b63",
+      //     tertiary: "#84a59d",
+      //     highlight: "rgba(143, 159, 169, 0.15)",
+      //     textHighlight: "#fff23688",
+      //   },
+      //   darkMode: {
+      //     light: "#161618",
+      //     lightgray: "#393639",
+      //     gray: "#646464",
+      //     darkgray: "#d4d4d4",
+      //     dark: "#ebebec",
+      //     secondary: "#7b97aa",
+      //     tertiary: "#84a59d",
+      //     highlight: "rgba(143, 159, 169, 0.15)",
+      //     textHighlight: "#b3aa0288",
+      //   },
+      // },
       colors: {
         lightMode: {
           light: "#faf8f8",
@@ -51,7 +75,6 @@ const config: QuartzConfig = {
           highlight: "rgba(143, 159, 169, 0.15)",
           textHighlight: "#b3aa0288",
         },
-      },
     },
   },
   plugins: {
